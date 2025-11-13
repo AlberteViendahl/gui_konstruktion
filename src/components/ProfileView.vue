5a8521b02587bbf4231df098b01207d921d0495a
Sar-Eib <sarah.eibye@gmail.com>
2025-11-13 11:56:12 +0100

    Structure and styling of ProfileInfoCard, with user-data for Soren and image upload of soren.jpg, w- current styling of ProfileView and inserted ProfileView component on profile page

---
+++ src/components/ProfileView.vue
@@ -0,0 +1,31 @@
+<script>
+import ProfileInfoCard from './ProfileInfoCard.vue';
+
+export default {
+  name: 'ProfileView',
+  components: {
+    ProfileInfoCard,
+  },
+  data() {
+    return {
+      user: {
+        id: 111,
+        name: 'Søren Nielsen',
+        address: 'Lysalleen 117, Roskilde',
+        phone: '+45 40 81 00 32',
+        email: 'soren.nielsen@eksempel.dk',
+        photoUrl: '/img/soren.jpg' 
+      }
+    };
+  }  
+}
+</script>
+
+<template>
+  <ProfileInfoCard :user="user" />
+</template>
+
+
+<style scoped>
+
+</style>
--- src/pages/Profil.vue
+++ src/pages/Profil.vue
@@ -1,7 +1,22 @@
-<script></script>
+<script>
+import ProfileView from '@/components/ProfileView.vue';
+export default {
+  name: 'profile',
+  components: {
+    ProfileView,
+  },
+  methods: {
+   
+  },
+  mounted() {
+    console.log('hello from profilepage')
+  }
+}
+</script>
 
 <template>
   <h1>Min Profil</h1>
+  <ProfileView />
 </template>
 
 <style lang="scss" scoped>
