a01b26101a561ff13d54e3305ea96bae033787fe
Sar-Eib <sarah.eibye@gmail.com>
2025-11-14 01:30:10 +0100

    Added fallback page for dead-end links and added routing to page

---
+++ src/pages/FallbackPage.vue
@@ -0,0 +1,15 @@
+<script>
+import PageHeader from '@/components/PageHeader.vue';
+export default {
+  name: "Liked",
+  components: {
+    PageHeader,
+  }
+};
+</script>
+
+<template>
+  <PageHeader title="Findes ikke" />
+</template>
+
+<style lang="scss" scoped></style>
