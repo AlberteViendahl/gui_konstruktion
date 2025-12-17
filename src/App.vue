732a28eaf26c37c16a9e84d8a1449e5289372d97
Sar-Eib <sarah.eibye@gmail.com>
2025-12-18 00:44:57 +0100

    Desktop navigationsbar with logo AND added additional logo for REBUILD

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <!-- Options API: https://vuejs.org/guide/introduction.html -->
 <script>
 import BottomBar from "./components/BottomBar.vue";
+import DesktopTopBar from "./components/DesktopTopBar.vue";
 
 export default {
   name: "App",
   components: {
     BottomBar,
+    DesktopTopBar,
   },
 };
 </script>
 
 <template>
+  <DesktopTopBar class="d-none d-lg-flex" />
   <div class="container main-container">
     <RouterView />
   </div>
-  <BottomBar />
+  <BottomBar class="d-lg-none" />
 </template>
 
 <style lang="scss">
---
+++ src/components/DesktopTopBar.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { useRoute } from "vue-router";
+
+import logo from "@/assets/icons/logo-notfilled.svg";
+import logoFilled from "@/assets/icons/logo-filled.svg";
+import search from "@/assets/icons/search.svg";
+import searchFilled from "@/assets/icons/search-fill.svg";
+import like from "@/assets/icons/like.svg";
+import likeFilled from "@/assets/icons/heart-fill.svg";
+import profil from "@/assets/icons/profil.svg";
+import profilFilled from "@/assets/icons/profil-fill.svg";
+import bigLogo from "@/assets/icons/bigLogo.svg";
+import bigtype from "@/assets/icons/big-logo-type.svg";
+
+const route = useRoute();
+</script>
+
+<template>
+  <nav id="top-bar" class="bg-primary position-fixed d-flex flex-row justify-content-between">
+    <!-- Forside -->
+    <RouterLink to="/">
+      <img class="main-logo p-2 ms-4" :src="bigtype" alt="Logo" />
+    </RouterLink>
+
+    <section class="d-flex flex-row justify-content-between gap-5 me-5">
+      <RouterLink to="/search" class="d-flex justify-content-between align-items-center">
+        <img
+          :src="route.path === '/search' ? searchFilled : search"
+          alt="Søgning"
+        />
+        <p>Udforsk</p>
+      </RouterLink>
+
+    
+      <RouterLink to="/liked" class="d-flex justify-content-between align-items-center">
+        <img
+          :src="route.path === '/liked' ? likeFilled : like"
+          alt="Synes godt om"
+        />
+        <p>Synes godt om</p>
+      </RouterLink>
+
+    
+      <RouterLink to="/profil" class="d-flex justify-content-between align-items-center">
+        <img
+          :src="route.path === '/profil' ? profilFilled : profil"
+          alt="Profil"
+        />
+        <p>Min profil</p>
+      </RouterLink>
+    </section>
+
+    
+  </nav>
+</template>
+
+<style scoped>
+#top-bar {
+  position: absolute;
+  top: 0;
+  z-index: 2;
+  width: 100%
+}
+
+p {
+  color: white;
+  margin: 0;
+}
+#top-bar section img {
+  width: 28px;
+  height: 28px;
+  filter: brightness(0) invert(1); /* gør ikonerne hvide */
+  opacity: 0.9;
+  transition: all 0.2s ease;
+  cursor: pointer;
+  margin-right: 10px;
+}
+#top-bar section img:hover {
+  transform: scale(1.15);
+}
+.main-logo {
+  height: 60px;
+  width: auto;
+}
+.router-link-active {
+  text-decoration: none;
+  p {
+    border-bottom: 2px solid white;
+  }
+}
+</style>
