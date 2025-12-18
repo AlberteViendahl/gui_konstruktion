21a6a03acb5b68db9635a61919f899fce9aaff93
Sar-Eib <sarah.eibye@gmail.com>
2025-12-18 01:40:04 +0100

    ADDED new grid layout for liked products (same as profileMySales grid) NEEDS API connection when database is set up

--- src/pages/Liked.vue
+++ src/pages/Liked.vue
@@ -2,20 +2,63 @@
   <PageHeader title="Synes godt om" class="d-lg-none" />
   <div class="container pt-5 mt-5 ">
     <div class="row">
-      <div class="col-6 p-1" v-for="p in likedProducts" :key="p.id">
-        <ProductCard
-          :id="p.id"
-          :title="p.title"
-          :image="p.image"
-          :price="p.price"
-          :quantity="p.quantity"
-          :material="p.material"
-          :initial-liked="true"
-        />
-      </div>
+      <ul class="product-grid list-unstyled">
+      <li class="" v-for="p in likedProducts" :key="p.id">
+          <ProductCard
+            :id="p.id"
+            :title="p.title"
+            :image="p.image"
+            :price="p.price"
+            :quantity="p.quantity"
+            :material="p.material"
+            :initial-liked="true"
+          />
+      </li>
+    </ul>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+
+.product-layout-section {
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    width: 60vw;
+  }
+}
+
+.product-grid {
+  display: grid;
+  gap: 15px;
+  justify-content: center; 
+  justify-items: center;
+  padding: 0;
+
+  grid-template-columns: repeat(1, 1fr);
+
+  @media (min-width: 485px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 992px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (min-width: 1200px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+
+li {
+  width: 100%;
+  max-width: 350px;
+}
+</style>
 
 <script>
 import PageHeader from '@/components/PageHeader.vue';
